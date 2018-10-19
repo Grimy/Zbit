632698971f1b9e8a5b9fcbd65a2b624a3933e411
Victor Adam <victor.adam@derpymail.org>
2018-10-19 17:09:24 +0200

    42

--- gate.pl
+++ gate.pl
@@ -1,37 +1,42 @@
 #!perl -l
+# gate.pl: find how to implement logical gates with as few traps as possible
 
-my %gates = ();
-my @names = qw(0 & > A < B ^ |);
+use strict;
+use warnings;
+no warnings qw(misc);
 
-my @trans = qw(
-	0  0  0  0  0  0  0  0
-	1  1  1  0  1  1  1  3
-	1  2  0  2  2  1  4  2
-	4  3  0  3  3  1  4  3
-	4  0  4  4  2  4  4  4
-	5  5  1  2  5  5  6  5
-	6  1  6  4  5  6  6  7
-	7  3  4  7  7  6  7  7
+# Transition table. Rows are traps, columns are states.
+# State 0: both dead
+# State 1: left
+# State 2: right
+# State 3: both alive
+# State 4: middle, facing right
+# State 5: middle, facing left
+# State 6: left+4
+# State 7: right+5
+my %traps = (
+	U => sub { y/1-7/1232167/ },  # middle omnibounce
+	T => sub { y/1-7/0224557/ },  # kill left
+	E => sub { y/1-7/1230012/ },  # kill middle
+	2 => sub { y/1-7/1014564/ },  # kill right
+	X => sub { y/1-7/4274567/ },  # bounce left to middle
+	M => sub { y/1-7/1232237/ },  # bounce middle to right
+	Y => sub { y/1-7/1231163/ },  # bounce middle to left
+	N => sub { y/1-7/1564567/ },  # bounce right to middle
 );
 
-gate: for (1..8**7) {
-	$_ = sprintf '%o', $_;
-	my @gate = map int, /./g;
-	my $gate = y/0-7/UTE2XMYN/r;
-	my @base = (1, 4, 6);
-	my @results = (0, 0);
+# Maps logical gates to the shortest sequence of traps implementing them.
+my %gates = ("123" => "");
+my @q = ("123");
 
-	for $state (@base) {
-		$state = $trans[8*$state + $_] for @gate;
-		next gate if $state == 2 || $state == 3 || $state == 5 || $state == 7;
-		$results[0] = 2 * $results[0] + ($state == 4 || $state == 6);
-		$results[1] = 2 * $results[1] + ($state == 1 || $state == 6);
+while (my $gate = shift @q) {
+	for my $trap (keys %traps) {
+		$_ = $gate;
+		$traps{$trap}->();
+		next if defined $gates{$_};
+		push @q, $_;
+		$gates{$_} = $gates{$gate} . $trap;
+		my $name = pack('b*b*', $_, y/12/21/r) =~ y//0><^&AB/cr;
+		print "$name $gates{$_}" if !/[4-7]/;
 	}
-
-	my $name = join '', @names[@results];
-	if (!$gates{$name}) {
-		$gates{$name} = $gate;
-		print "$name $gate";
-	}
-
 }
